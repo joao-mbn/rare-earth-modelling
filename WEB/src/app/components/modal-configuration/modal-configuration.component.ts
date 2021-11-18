ac101780c7f04141df11b5f997734a6540488e2d
João Nascimento <nascimento.j.1@eq.ufrj.br>
2021-11-17 21:37:33 -0300

    Further modal form building

--- WEB/src/app/components/modal-configuration/modal-configuration.component.ts
+++ WEB/src/app/components/modal-configuration/modal-configuration.component.ts
@@ -2,7 +2,7 @@ import { Uom } from 'src/app/contracts/Interfaces/Uom';
 import { SummaryOperationalVariable } from './../../contracts/Interfaces/OperationalVariable';
 import { ElementProperties } from './../../contracts/Interfaces/ElementProperties';
 import { ValidationService } from './../../services/validation.service';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Form, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
@@ -20,21 +20,13 @@ import * as defaultOptions from '../../../mocks/defaultOptions';
 })
 export class ModalConfigurationComponent implements OnInit {
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private validationService: ValidationService,
-    private projectService: ProjectService,
-    public dialogRef: MatDialogRef<ModalConfigurationComponent>,
-    @Inject(MAT_DIALOG_DATA) public data?: { project: Project },
-  ) { }
-
   project!: Project;
   displayDefault!: boolean;
 
   projectName!: string;
   economicVariables!: Material[];
   modelConstants!: ElementProperties[];
-  operationalVariable!: SummaryOperationalVariable[];
+  operationalVariables!: SummaryOperationalVariable[];
 
   uoms: Uom[] = defaultOptions.UOMS;
   materials: Material[] = defaultOptions.MATERIALS;
@@ -45,13 +37,26 @@ export class ModalConfigurationComponent implements OnInit {
   volumetricPriceUoms!: OptionToDropdown[];
   unitPriceUoms!: OptionToDropdown[];
 
-  ngOnInit(): void {
+  forms!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private validationService: ValidationService,
+    private projectService: ProjectService,
+    public dialogRef: MatDialogRef<ModalConfigurationComponent>,
+    @Inject(MAT_DIALOG_DATA) public data?: { project: Project },
+  ) { }
+
+  ngOnInit(): void {
     this.addSelections();
     this.separateByType();
 
-    this.data ? this.destructureProjectConfigurations() : this.buildProjectTemplate();
+    this.data ? this.destructureProject() : this.buildProjectTemplate();
+  }
 
+  private addSelections(): void {
+    this.uoms.forEach(uom => uom.isSelected = uom.isSelected ?? false);
+    this.materials.forEach(material => material.isSelected = material.isSelected ?? false);
   }
 
   private separateByType(): void {
@@ -62,10 +67,18 @@ export class ModalConfigurationComponent implements OnInit {
     this.etrs = this.materials.filter(material => material.type === 'etr').map(material => { return { value: material.longString, id: material.materialId, disabled: false }; });
   }
 
-  private addSelections(): void {
-    this.uoms.forEach(uom => uom.isSelected ?? false);
-    this.materials.forEach(material => material.isSelected ?? false);
+  private buildProjectTemplate(): Project {
+    return PROJECT;
   }
+
+  private destructureProject(): void {
+    this.data = this.data as { project: Project };
+    this.projectName = this.data.project.longString;
+    this.modelConstants = this.data.project.projectConfiguration.modelConstants;
+    this.operationalVariables = this.data.project.projectConfiguration.operationalVariables;
+    this.economicVariables = this.data.project.projectConfiguration.economicVariables;
+  }
+
 
   public onSave(): void {
     //TODO implement
@@ -83,10 +96,11 @@ export class ModalConfigurationComponent implements OnInit {
 
   public onSelectEtr(option: OptionToDropdown): void {
 
-    //TODO build model constants for that element
-    //TODO add to the economic variables
     const etr = this.materials.find(material => material.materialId === option.id);
-    if (etr) { etr.isSelected = etr.isSelected ? false : true };
+    if (etr) {
+      etr.isSelected = etr.isSelected ? false : true;
+      etr.isSelected ? this.addEtrs(etr) : this.removeEtrs(etr);
+    };
 
   }
 
@@ -94,24 +108,61 @@ export class ModalConfigurationComponent implements OnInit {
     this.dialogRef.close(this.data);
   }
 
-  private buildProjectTemplate(): Project {
-    return PROJECT;
-  }
-
-  private destructureProjectConfigurations(): void {
-    this.data = this.data as { project: Project };
-    this.projectName = this.data.project.longString;
-    this.modelConstants = this.data.project.projectConfiguration.modelConstants;
-    this.operationalVariable = this.data.project.projectConfiguration.operationalVariables;
-    this.economicVariables = this.data.project.projectConfiguration.economicVariables;
-  }
-
-
   private buildForms(): void {
-    this.formBuilder.group({});
-    const mock1 = new FormControl('', Validators.required);
-    const mock2 = new FormGroup({});
+    this.forms = this.formBuilder.group({
+      etrOptions: '',
+      modelConstants: this.formBuilder.array([]),
+      operationalVariables: this.formBuilder.group(this.buildOperationalVariablesForms()),
+      economicVariables: this.formBuilder.array([]),
+    });
+    this.buildOperationalVariablesForms();
   }
+
+  private addEtrs(etr: Material): void {
+    const modelConstant = this.modelConstants.find(constant => constant.materialId === etr.materialId);
+    const a = modelConstant?.defaultProperties.find(property => property.description === 'a')?.value;
+    const b = modelConstant?.defaultProperties.find(property => property.description === 'b')?.value;
+    const initialAqueousConcentration = modelConstant?.defaultProperties.find(property => property.description === 'Initial Aqueous Concentration');
+    const initialOrganicConcentration = modelConstant?.defaultProperties.find(property => property.description === 'Initial Organic Concentration');
+    const modelConstantToForm = this.formBuilder.group({
+      etr: modelConstant?.symbol,
+      a: a,
+      b: b,
+      initialAqueousConcentration: initialAqueousConcentration?.value,
+      initialAqueousConcentrationUom: initialAqueousConcentration?.uomLongString,
+      initialOrganicConcentration: initialOrganicConcentration?.value,
+      initialOrganicConcentrationUom: initialOrganicConcentration?.uomLongString
+    })
+
+    const economicVariable = etr;
+    const price = economicVariable.defaultProperties.find(property => property.propertyLongString === 'Price');
+    const economicVariableToForm = this.formBuilder.group({ etr: economicVariable.symbol, price: price?.value, priceUom: price?.uomLongString });
+
+    (this.forms.get('modelConstants') as FormArray).push(modelConstantToForm);
+    (this.forms.get('economicVariables') as FormArray).push(economicVariableToForm);
+  }
+
+  private removeEtrs(etr: Material): void {
+    const modelConstantsArray = (this.forms.get('modelConstants') as FormArray);
+    Array(modelConstantsArray.length).some((i) => {
+      const etrToDelete = this.forms.get(['modelConstants', 0, 'etr'])?.value as string;
+      if (etrToDelete === etr.symbol) {
+        modelConstantsArray.removeAt(i);
+        return true;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  private buildOperationalVariablesForms(): { [name: string]: number[] } {
+    let operationalVariablesForms: { [name: string]: number[] } = {};
+    this.operationalVariables.forEach((variable) => {
+      Object.assign(operationalVariablesForms, { [variable.name]: variable.value })
+    })
+    return operationalVariablesForms;
+  }
+
 
 
 }
